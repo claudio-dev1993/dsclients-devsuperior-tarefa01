8ff3ef19110aa171ed402195ae5f6691141ad04a
claudio-dev1993 <claudinhoroberto2008@gmail.com>
2022-01-27 23:04:57 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dstarefa01/Dstarefa01Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dstarefa01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dstarefa01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dstarefa01Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dstarefa01/Dstarefa01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dstarefa01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dstarefa01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
