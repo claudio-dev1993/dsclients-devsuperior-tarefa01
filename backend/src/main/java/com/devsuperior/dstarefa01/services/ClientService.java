9e548320ba6a9b10ce534448b4512de803927bba
claudio-dev1993 <claudinhoroberto2008@gmail.com>
2022-02-02 19:02:20 -0300

    Resource and service update methods

--- backend/src/main/java/com/devsuperior/dstarefa01/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dstarefa01/services/ClientService.java
@@ -2,6 +2,8 @@ package com.devsuperior.dstarefa01.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -31,16 +33,36 @@ public class ClientService {
 		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found!"));
 		return new ClientDTO(entity);
 	}
-	
+
 	@Transactional(readOnly = true)
 	public ClientDTO insert(ClientDTO dto) {
-		Client client = new Client();
-		client.setName(dto.getName());
-		client.setBirthDate(dto.getBirthDate());
-		client.setChildren(dto.getChildren());
-		client.setCpf(dto.getCpf());
-		client.setIncome(dto.getIncome());
+		Client client = new Client(dto.getName(), dto.getCpf(), dto.getIncome(), dto.getBirthDate(), dto.getChildren());
 		client = repository.save(client);
 		return new ClientDTO(client);
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client client = repository.getOne(id);
+			client.setName(dto.getName());
+			client.setBirthDate(dto.getBirthDate());
+			client.setChildren(dto.getChildren());
+			client.setCpf(dto.getCpf());
+			client.setIncome(dto.getIncome());
+			client = repository.save(client);
+			return new ClientDTO(client);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+	
+	@Transactional(readOnly = true)
+	public void delete(Long id) {
+		try {
+			
+		} catch (Exception e) {
+			
+		}
 	}
 }
